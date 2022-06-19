7ce496b0f1948b0439487e3d1bb7a0413937c31d
bnun <85907829+bnunowo@users.noreply.github.com>
2022-06-19 08:15:10 -0700

    Add files via upload

---
+++ Windows-Source-Code-Install.py
@@ -0,0 +1,31 @@
+from fileinput import close
+import os
+from time import sleep
+from urllib import request, response
+from xmlrpc.client import ResponseError
+from colorama import Fore
+
+
+os.system("cls && title MICROSOFT")
+
+def Design():
+    print(f"""
+
+{Fore.BLUE}    .----------------.
+{Fore.BLUE}   |          _       |
+{Fore.BLUE}   |      _.-'|'-._   |
+{Fore.BLUE}   | .__.|    |    |  |
+{Fore.BLUE}   |     |_.-'|'-._|  |
+{Fore.BLUE}   | '--'|    |    |  |
+{Fore.BLUE}   | '--'|_.-'`'-._|  |
+{Fore.BLUE}   | '--'          `  |
+{Fore.BLUE}    '----------------'
+
+ {Fore.GREEN}Microsoft Windows 11 Source Code is now installed")
+    """)
+
+Design()
+
+os.system('pause')
+
+os.system('git clone "https://github.com/bnunowo/Windows-11-SOURCE-CODE.git')
